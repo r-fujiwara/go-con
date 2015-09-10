123e5e092145151e78d866bb7d2b12a4561c26e4
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-09-10 11:23:52 +0900

    add pry

--- vol1/sample-sinatra/app.rb
+++ vol1/sample-sinatra/app.rb
@@ -1,5 +1,6 @@
 require "sinatra/base"
 require "sinatra/reloader"
+require "pry"
 
 class App < Sinatra::Base
   configure :development do
@@ -8,7 +9,11 @@ class App < Sinatra::Base
 
 
   get '/' do
+    puts "fuga"
     'Hello world'
   end
+
+  run!
 end
+
 
